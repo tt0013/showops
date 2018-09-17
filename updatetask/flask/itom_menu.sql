61faa113796b28da367c6fcb1c0d48b4bd4321b4
h <tt06090013@163.com>
2018-09-17 19:15:09 +0800

    code rsync

---
+++ updatetask/flask/itom_menu.sql
@@ -0,0 +1,3 @@
+INSERT INTO `itom_menu` VALUES (8, '程序更新', 0, '', 0, 2, 0, 'layui-icon-set', 0);
+INSERT INTO `itom_menu` VALUES (9, '邮件发送', 8, '/itom/updatemail/', 0, 0, 0, '', 0);
+INSERT INTO `itom_menu` VALUES (10, '执行操作', 8, '/itom/async/', 0, 0, 0, '', 0);
